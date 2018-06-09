0ac909ea3314ecd0acbe26f455d279068c73bdfe
indianbhaji <indianbhaji@gmail.com>
2018-06-09 20:26:52 +0100

    complete overhaul of retriever; support of parameters

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
-# data retriever for FlightRadar24
-Intended for openscope use.
+# Data retriever for FlightRadar24
+Intended for OpenScope use.
 
 To use: 
 1. clone the repository locally
-2. `npm install` 
-3. locate the line `const airportCode = "DXB"` and replace DXB with a 3-letter IATA airport code or 4-letter ICAO code
-4. `npm run retrieve`
-5. your output will be the `output` folder, named airportCode.json
+2. `npm install`
 
-The output will require some formatting.
+# `npm run retrieve airport [--] [--aircraft] [--origin] [--destination]`
+**Required**: `airport` (3 letter IATA or 4 letter ICAO airport code)
+
+**Optional**:
+- -- after `airport` parameter is required if the following parameters are used (ie. `npm run retrieve dxb -- --aircraft`) 
+- --aircraft (aircraft type for each flight recorded is given) 
+- --origin (origin airport for arrivals is given)
+- --destination (destination airport for departures is given)
+
+If the command `npm run retrieve airport` is run without parameters, a file with the frequency of all airlines flying in/out of the airport is given.
+
+**Example command**: 
+`npm run retrieve DXB --aircraft --destination` retrieves flights in/out of Dubai over from 0000z to 2359z, returning the frequency that an airline operates an aircraft type into Dubai, and the frequency an airline operates an aircraft type out to a destination
+
+The output will require some formatting; it is recommended that an editor such as Atom, Visual Studio Code or Sublime Text is used to bulk process and format a file through the usage of the multi-cursor feature.
--- dataRetriever.js
+++ dataRetriever.js
@@ -1,241 +1,229 @@
-const fs = require('fs')
+const fs = require('fs');
 const request = require('request');
-
-function filterFleets(airlineFleets, state) {
-    for (i = 0; i < airlineFleets.length; i++) {
-        let index = airlineFleets.indexOf(airlineFleets[i]),
-            counter = 0
-
-        while (index != -1) {
-            counter++;
-            index = airlineFleets.indexOf(airlineFleets[i], index + 1)
-        }
-
-        finalArray[state].push([airlineFleets[i], counter])
-
-        airlineFleets = airlineFleets.filter(function(value) {
-            if (value == airlineFleets[i]) {
-                return false
-            }
-            return true
-        })
-
-        finalArray[state].sort()
-
-        i--
-    }
-}
+const _ = require('lodash');
+const parseArguments = require('minimist');
 
 class Flight {
-    constructor(flightDetails, airportCode) {
-        this.identification = flightDetails['identification']
-        this.airlineIcao = flightDetails['airline']['code']['icao'] || ""
+    constructor(flightDetails) {
+        this.identification = flightDetails.identification;
+        this.airline = flightDetails.airline;
+        this.aircraft = flightDetails.aircraft;
+        this.status = flightDetails.status;
+        this.airport = flightDetails.airport;
 
-        this.callsign = flightDetails['identification']['callsign'] || this.airlineIcao;
+        this.callsign = this.identification && this.identification.callsign;
+        this.airlineIcao = this.airline && this.airline.code.icao;
+        this.aircraftIcao = this.aircraft && this.aircraft.model.code;
+        this.genericTimestamp = flightDetails.status.generic.eventTime.utc;
 
-        this.aircraftIcao = flightDetails['aircraft']['model']['code'];
+        this.flightType = this.status.generic.status.type;
 
-        this.genericTimestamp = flightDetails['status']['generic']['eventTime']['utc'];
-
-        this.scheduledDeparture = flightDetails['time']['scheduled']['departure']
-        || flightDetails['time']['real']['departure']
-        || flightDetails['time']['estimated']['departure']
-        || genericTimestamp;
-
-        this.scheduledArrival = flightDetails['time']['scheduled']['arrival']
-        || flightDetails['time']['real']['arrival']
-        || flightDetails['time']['estimated']['arrival']
-        || genericTimestamp;
-
-
-
-        this.flightType = flightDetails['status']['generic']['status']['type'];
-
-        this.origin
-        this.destination
-
-        if (this.flightNumber === 'departure') {
-            this.destination = flightDetails['airport']['destination']['code']['icao'] || airportCode
-        } else {
-            this.origin = flightDetails['airport']['origin']['code']['icao'] || airportCode
-        }
+        this.origin = this.airport.origin.code && this.airport.origin.code.icao;
+        this.destination =  this.airport.destination.code && this.airport.destination.code.icao;
 
         if (this.callsign) {
-            this.airlineIcao = this.callsign.substring(0, 3);
+            const regex = '^[a-z][a-z][a-z]$';
+
+            if (this.callsign.substring(0, 3).match(regex)) {
+                this.airlineIcao = this.callsign.substring(0, 3);
+            }
         }
     }
 
-    createArray(airlineWithFleet) {
-        const output = [`${this.airlineIcao}/${this.aircraftIcao}`.toLowerCase(), this.origin || this.destination]
-            || `${this.airlineIcao}/${this.aircraftIcao}`.toLowerCase()
-            || `${this.airlineIcao}`;
+    getData(args) {
+        const [aircraftType, origin, destination] = args;
+        let completeString = this.airlineIcao;
 
-        return [`${this.airlineIcao}/${this.aircraftIcao}`.toLowerCase(), this.origin || this.destination];
+        if (aircraftType) { completeString = `${completeString}/${this.aircraftIcao}`; }
+        if (origin && this.origin) { completeString = `${completeString}/${this.origin}`; }
+        if (destination && this.destination) { completeString = `${completeString}/${this.destination}`; }
+
+        return completeString;
     }
-
 }
 
 function fetchData(airportCode, timestamp, pageNumber, callback) {
-    const link = `https://api.flightradar24.com/common/v1/airport.json?code=${airportCode}&plugin[]=&plugin-setting[schedule][mode]=&plugin-setting[schedule][timestamp]=${timestamp}&page=${pageNumber}&limit=100&token=`
+    const link = `https://api.flightradar24.com/common/v1/airport.json?code=${airportCode}&plugin[]=&plugin-setting[schedule][mode]=&plugin-setting[schedule][timestamp]=${timestamp}&page=${pageNumber}&limit=100&token=`;
 
     request(link, (error, response, body) => {
         if (error) {
-            throw new TypeError(error);
+            throw new Error(error);
         }
 
         if (response && response.statusCode === 400) {
-            throw new TypeError(JSON.parse(body)['errors']['message'])
+            const parsedBody = JSON.parse(body);
+            const flightRadarError = parsedBody.errors.message;
+
+            throw new Error(flightRadarError);
         }
 
-        callback(JSON.parse(body))
-    })
+        callback(JSON.parse(body));
+    });
 }
 
 function segregateFlightTypes(data) {
-    const arrivals = data['result']['response']['airport']['pluginData']['schedule']['arrivals']['data']
-    const departures = data['result']['response']['airport']['pluginData']['schedule']['departures']['data']
+    const arrivals = data.result.response.airport.pluginData.schedule.arrivals.data;
+    const departures = data.result.response.airport.pluginData.schedule.departures.data;
 
-    return [
-        arrivals,
-        departures
-    ]
+    return [departures, arrivals];
 }
 
-function constructFlightDetailsArray(data, lowerTimestamp, upperTimestamp, airportCode) {
+function constructFlightArray(args) {
+    const [data, lowerTimestamp, upperTimestamp] = args;
+
     const flightDetailsArray = [];
-    let dataComplete = false,
-        i = 0;
+    let dataComplete;
+    let i = 0;
 
     while (i < data.length && !dataComplete) {
-        let flightDetails = data[i]['flight'],
-            flightType = flightDetails['status']['generic']['status']['type'],
-            genericFlightTimestamp = flightDetails['status']['generic']['eventTime'['utc']],
-            timestamp = flightDetails['time']['scheduled'][flightType]
-                || flightDetails['time']['real'][flightType]
-                || flightDetails['time']['estimated'][flightType]
-                || genericFlightTimestamp;            
+        const flightDetails = data[i].flight;
+        const status = flightDetails.status.generic;
+        const flightType = status.status.type;
+        const genericTimeStamp = status.eventTime.utc;
+
+        const scheduledTimestamp = flightDetails.time.scheduled[flightType];
+        const actualTimestamp = flightDetails.time.real[flightType];
+        const estimatedTimestamp = flightDetails.time.estimated[flightType];
+
+        const timestamp = scheduledTimestamp || actualTimestamp || estimatedTimestamp || genericTimeStamp; // eslint-disable-line max-len
 
         if (lowerTimestamp <= timestamp && timestamp < upperTimestamp) {
-            flightDetailsArray.push(
-                new Flight(flightDetails, airportCode)
-            )
+            flightDetailsArray.push(new Flight(flightDetails));
+            dataComplete = false;
         } else {
-            dataComplete = true
+            dataComplete = true;
         }
 
         i++;
     }
 
-    return [
-        flightDetailsArray,
-        dataComplete
-    ]
+    if (dataComplete == null) {
+        dataComplete = true;
+    }
+
+    return [flightDetailsArray, dataComplete];
 }
 
-function exportAirlines(departuresArray, arrivalsArray, airportCode) {
-    const forceArrayOneLine = function(key, value) {
-        if (typeof value === 'object' && value.length === 3) {
-            if (typeof value[0] === 'string' && typeof value[1] === 'string') {
-                return JSON.stringify(value)
+function extractRequiredInfo(args) {
+    const [flightArray, getAircraft, getOrigin, getDestination] = args;
+    const resultingArray = [];
+    for (let i = 0; i < flightArray.length; i++) {
+        const args = [getAircraft, getOrigin, getDestination];
+
+        resultingArray.push(flightArray[i].getData(args));
+    }
+
+    return resultingArray;
+}
+
+function collectLikeTerms(array) {
+    const i = 0;
+    const endArray = [];
+    let flightArray = array;
+
+    while (flightArray.length !== 0) {
+        const currentValue = flightArray[i];
+        const arrayLength = flightArray.length;
+
+        flightArray = _.without(flightArray, currentValue);
+
+        const newArrayLength = flightArray.length;
+        const frequency = arrayLength - newArrayLength;
+
+        if (currentValue) {
+            endArray.push([currentValue, frequency]);
+        }
+    }
+
+    return endArray;
+}
+
+function exportAirlines(departureArray, arrivalArray, airportIcao) {
+    const forceArrayOneLine = (key, value) => {
+        if (typeof value === 'object') {
+            if (value && typeof value[0] === 'string' && typeof value[1] === 'number') {
+                return `[${value}]`;
             }
         }
 
-        return value
+        return value;
+    };
+
+    const exportObject = JSON.stringify({
+        departures: departureArray.sort(),
+        arrivals: arrivalArray.sort(),
+    }, forceArrayOneLine, 4).toLowerCase();
+
+
+    if (!fs.existsSync('./output')) {
+        fs.mkdirSync('output');
     }
 
-    const exportObject = {
-        "departures": departuresArray.sort(),
-        "arrivals": arrivalsArray.sort()
-    }
+    const filePath = `./output/${airportIcao}.json`;
 
-    objectStringified = JSON.stringify(exportObject, forceArrayOneLine, 4)
-
-    if (!fs.existsSync('output')) {
-        fs.mkdirSync('output')
-    }
-
-    fs.writeFile(`output/${airportCode}.json`, objectStringified, (error) => {
+    fs.writeFile(filePath, exportObject, error => {
         if (error) {
-            throw new TypeError(error)
+            throw new TypeError(error);
         }
 
-        console.log(`Saved as ./output/${airportCode}.json`)
-    })
+        console.log(`Saved as ${filePath}!`);
+    });
 }
 
-function extractNeccessaryInfo(flightArray) {
-    let resultingArray = []
+const args = parseArguments(process.argv.slice(2));
+const airportIcao = args._[0] || 0;
+const includeAircraft = args.aircraft || false;
+const includeOrigin = args.origin || false;
+const includeDestination = args.destination || false;
+console.log(`options: 
+    airport: ${airportIcao}
+    include aircraft type: ${includeAircraft}
+    include origin: ${includeOrigin}
+    include destination: ${includeDestination}
+`);
 
-    for (i = 0; i < flightArray.length; i++) {
-        resultingArray.push(flightArray[i].createArray())
-    }
-    return resultingArray
+if (airportIcao.length > 4 || airportIcao < 3) {
+    throw new Error('Airport parameter must be 3 letter IATA or 4 letter ICAO');
 }
 
-function collectLikeArrays(flightArray) {
-    const i = 0
-    let endArray = []
+const dateObj = new Date();
+const startTimestamp = Math.round(dateObj.setUTCHours(0, 0, 0, 0) / 1000);
+const endTimestamp = startTimestamp + 86400;
 
-    while (flightArray.length != 0) {
-        let counter = 0
-        let currentArray = flightArray[i]
-        let index = flightArray.indexOf(flightArray[i])
+let departuresArray = [];
+let arrivalsArray = [];
+let pageNumber = 0;
 
-        flightArray = flightArray.filter((value) => {
-            let compare = currentArray.toString()
-            value = value.toString()
-            
-            if (value != compare) {
-                return true
-            } else {
-                counter++;
-                return false
-            }
-        })
-
-        endArray.push([...currentArray, counter])
-    }
-    return endArray
-}
-
-const airportCode = "DXB",
-    dateObj = new Date(),
-    startTimestamp = Math.round(dateObj.setUTCHours(0, 0, 0, 0) / 1000),
-    endTimestamp = startTimestamp + 86400;
-
-let departuresArray = [],
-    arrivalsArray = [],
-    pageNumber = 0;
-
-incrementPageNumber = function () {
+const incrementPageNumber = function() {
     pageNumber++;
-    console.log(`Retrieving page: ${pageNumber}`)
+    console.log(`Retrieving page: ${pageNumber}`);
 
-    const jsonRetrieved = function(parsedJson) {
-        let segregatedTypes = segregateFlightTypes(parsedJson)
-        let departures = segregatedTypes[0],
-            arrivals = segregatedTypes[1];
+    const jsonRetrieved = parsedJson => {
+        const [departures, arrivals] = segregateFlightTypes(parsedJson);
 
-        let flightDetailsArray = constructFlightDetailsArray(departures, startTimestamp, endTimestamp, airportCode),
-            departuresComplete = flightDetailsArray[1];
+        let args = [departures, startTimestamp, endTimestamp];
+        const [departuresDetailsArray, departuresComplete] = constructFlightArray(args);
 
-        departuresArray = [...departuresArray, ...flightDetailsArray[0]]
+        args = [arrivals, startTimestamp, endTimestamp];
+        const [arrivalsDetailsArray, arrivalsComplete] = constructFlightArray(args);
 
-        flightDetailsArray = constructFlightDetailsArray(departures, startTimestamp, endTimestamp, airportCode),
-            arrivalsComplete = flightDetailsArray[1],
-            arrivalsArray = [...arrivalsArray, ...flightDetailsArray[0]];
+        departuresArray = [...departuresArray, ...departuresDetailsArray];
+        arrivalsArray = [...arrivalsArray, ...arrivalsDetailsArray];
 
-        if (!arrivalsComplete && !departuresComplete) {
+        if (!arrivalsComplete || !departuresComplete) {
             incrementPageNumber();
         } else if (arrivalsComplete && departuresComplete) {
-            departuresArray = collectLikeArrays(extractNeccessaryInfo(departuresArray));
-            arrivalsArray = collectLikeArrays(extractNeccessaryInfo(arrivalsArray));
+            args = [departuresArray, includeAircraft, includeOrigin, includeDestination];
+            departuresArray = collectLikeTerms(extractRequiredInfo(args));
 
-            exportAirlines(departuresArray, arrivalsArray, airportCode);
+            args = [arrivalsArray, includeAircraft, includeOrigin, includeDestination];
+            arrivalsArray = collectLikeTerms(extractRequiredInfo(args));
+
+            exportAirlines(departuresArray, arrivalsArray, airportIcao);
         }
-    }
+    };
 
-    fetchData(airportCode, startTimestamp.toString(), pageNumber.toString(), jsonRetrieved);
-}
+    fetchData(airportIcao, startTimestamp, pageNumber, jsonRetrieved);
+};
 
-incrementPageNumber()
+incrementPageNumber();
